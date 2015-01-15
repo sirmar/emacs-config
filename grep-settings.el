6ad90d6e71b9b2398bbe5ae779bf8487f5d95adb
Marcus Veibäck <sirmar@gmail.com>
2015-01-15 17:14:01 +0100

    Center grep search results.

--- grep-settings.el
+++ grep-settings.el
@@ -9,6 +9,9 @@
 (defadvice grep (after delete-grep-header activate) (delete-grep-header))
 (defadvice rgrep (after delete-grep-header activate) (delete-grep-header))
 
+;; (setq grep-find-template "find . <X> -type f <F> -print0 | xargs -n 100 -0 -e grep <C> -nH -E <R>")
+(add-hook 'next-error-hook 'recenter)
+
 ;; Ignore directories and files
 (eval-after-load "grep"
   '(progn
