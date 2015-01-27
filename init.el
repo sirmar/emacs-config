33065fde81aeae6dea73e023f0163a11cb172a94
Marcus Veibäck <sirmar@gmail.com>
2015-01-27 13:55:59 +0100

    Use org file to init emacs.

--- init.el
+++ init.el
@@ -1,10 +1,1 @@
-(load "~/.emacs.d/pre-cask.el")
-
-(require 'cask "/usr/local/Cellar/cask/0.7.2/cask.el")
-(cask-initialize)
-
-(load "~/.emacs.d/cask-inits.el")
-(load "~/.emacs.d/settings.el")
-(load "~/.emacs.d/functions.el")
-(load "~/.emacs.d/key-bindings.el")
-(load "~/.emacs.d/grep-settings.el")
+(org-babel-load-file "~/.emacs.d/emacs_config.org")
