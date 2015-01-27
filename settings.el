0035ea3d1f5af2da4df95396b45bad3c52ea2a30
Marcus Veibäck <sirmar@gmail.com>
2015-01-27 13:22:00 +0100

    Org mode configs and make whitespace mode work.

--- settings.el
+++ settings.el
@@ -5,9 +5,9 @@
 (setq ediff-split-window-function (quote split-window-horizontally))
 
 ;; Whitespace
-(setq whitespace-line-column 100)
-(setq whitespace-style '(empty tabs trailing lines-tail))
 (global-whitespace-mode t)
+(setq whitespace-line-column 100)
+(setq whitespace-style '(face empty tabs trailing lines-tail indentation::space))
 (add-hook 'before-save-hook 'sanitize-whitespace)
 
 ;; Search/Replace
@@ -17,9 +17,30 @@
 (setq dabbrev-case-replace nil)
 (delete-selection-mode)
 
+;; Org mode
+(setq org-startup-indented t)
+(setq org-startup-truncated nil)
+(setq org-src-fontify-natively t)
+(add-hook 'text-mode-hook
+          (lambda ()
+            (setq org-default-notes-file (concat org-directory "/todo.org"))
+            (define-key global-map "\C-cc" 'org-capture)
+            ))
+(defface org-block-begin-line
+  '((t (:background "#DDDDDD")))
+  "Face used for the line delimiting the begin of source blocks.")
+
+(defface org-block-background
+  '((t (:background "#EEEEEE")))
+  "Face used for the source block background.")
+
+(defface org-block-end-line
+  '((t (:foreground "#111111" :background "#DDDDDD")))
+  "Face used for the line delimiting the end of source blocks.")
+
 ;; Misc
 (setq compile-command "")
 (setq tags-file-name "~/TAGS")
-(setq make-backup-files nil)
 (setq mouse-yank-at-point t)
+(setq make-backup-files nil)
 (defalias 'yes-or-no-p 'y-or-n-p)
