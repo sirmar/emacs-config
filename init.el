495b779cbf1e5188fafc93a3d8b18bea301666d6
Marcus Veibäck <sirmar@gmail.com>
2026-05-16 19:01:29 +0200

    Add TypeScript mode, XML config, exec-path-from-shell, Mac keybindings

--- init.el
+++ init.el
@@ -1,3 +1,16 @@
 (package-initialize)
 
 (org-babel-load-file "~/.emacs.d/emacs.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(treesit-auto zenburn-theme yasnippet yaml-mode which-key wgrep smex restclient rainbow-delimiters php-mode org-bullets multiple-cursors move-text magit lsp-ivy ivy-rich go-mode flycheck flx fill-column-indicator expand-region exec-path-from-shell doom-modeline dockerfile-mode discover dired-single dired-hide-dotfiles diminish dashboard counsel-projectile company-box all-the-icons-dired ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
